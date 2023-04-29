19d94b1165644dea8a4f5e80cb9d55a6c0447f2e
deep0072 <deepakumar50136@gmail.com>
2023-04-29 14:14:30 +0530

    get balance of account into format

--- frontend/etherejsScript.js
+++ frontend/etherejsScript.js
@@ -6,4 +6,19 @@ const provider = new ethers.JsonRpcProvider(url);
 console.log("block nunber", await provider.getBlockNumber());
 
 // if we want to get the real address by pasing ens name
-console.log("real addres", await provider.resolveName("vitalik.eth"));
+console.log("real address", await provider.resolveName("vitalik.eth"));
+
+// convert address to ens name
+
+console.log(
+  "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045 ens name is ",
+  await provider.lookupAddress("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045")
+);
+
+// get balance of perticular account in wei.
+
+let vitalikBalance = (await provider.getBalance("vitalik.eth")).toString();
+
+// if want to get exact value we can use here formatEther method .
+
+console.log("fromattedEther", ethers.formatEther(vitalikBalance));
